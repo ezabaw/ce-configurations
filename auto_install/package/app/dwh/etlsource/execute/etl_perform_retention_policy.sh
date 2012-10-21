993e19951796d160e3c65f709ce2105b7c233be9
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-21 17:45:28 +0000

    qnd: Fix hard coded /opt/kaltura paths.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@88930 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ auto_install/package/app/dwh/etlsource/execute/etl_perform_retention_policy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KITCHEN=/opt/kaltura/dwh/pentaho/pdi/kitchen.sh
+ROOT_DIR=@BASE_DIR@/dwh
+WHEN=$(date +%Y%m%d)
+
+while getopts "k:p:" o
+do	case "$o" in
+    k)	KITCHEN="$OPTARG";;
+    p)	ROOT_DIR="$OPTARG";;
+	[?])	echo >&2 "Usage: $0 [-k  pdi-path] [-p dwh-path]"
+		exit 1;;
+	esac
+done
+
+LOGFILE=$ROOT_DIR/logs/etl_perform_retention_policy-${WHEN}.log
+
+export KETTLE_HOME=$ROOT_DIR
+sh $KITCHEN /file $ROOT_DIR/etlsource/perform_retention_policy.kjb >> $LOGFILE 2>&1
---
+++ auto_install/package/app/dwh/etlsource/execute/etl_update_dims.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KITCHEN=/opt/kaltura/dwh/pentaho/pdi/kitchen.sh
+ROOT_DIR=@BASE_DIR@/dwh
+WHEN=$(date +%Y%m%d-%H)
+
+while getopts "k:p:" o
+do	case "$o" in
+    k)	KITCHEN="$OPTARG";;
+    p)	ROOT_DIR="$OPTARG";;
+	[?])	echo >&2 "Usage: $0 [-k  pdi-path] [-p dwh-path]"
+		exit 1;;
+	esac
+done
+
+LOGFILE=$ROOT_DIR/logs/etl_update_dims-${WHEN}.log
+
+export KETTLE_HOME=$ROOT_DIR
+sh $KITCHEN /file $ROOT_DIR/etlsource/dimensions/update_dimensions.kjb >> $LOGFILE 2>&1
