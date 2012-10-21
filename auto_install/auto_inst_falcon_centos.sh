25b08723eabc44df362dc3951e238baeb0390ee5
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-21 19:38:22 +0000

    qnd: today.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@88935 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- auto_install/auto_inst_falcon_centos.sh
+++ auto_install/auto_inst_falcon_centos.sh
@@ -7,24 +7,25 @@ else
 fi
 install_deps
 setup_pentaho
+set_selinux_permissive
+set_php_ini
 echo "Starting needed daemons.."
 for i in httpd memcached;do
     /etc/init.d/$i start
     chkconfig $i on
 done
-set_selinux_permissive
-echo "Setting request_order = CGP in php.ini"
-sed -i 's@request_order\s*=\s*.*@request_order = "CGP"@' /etc/php.ini
 cd $INSTALL_DIR && php install.php -s user_input.ini
 sed -i 's@ = @=@g' $INSTALL_DIR/user_input.ini
 . $INSTALL_DIR/user_input.ini
 # Add the "kaltura" user
 create_kalt_user
 fix_permissions
-sed -i 's@ root @ kaltura @' $BASE_DIR/crontab/kaltura_crontab
-sed -i 's@kaltura /usr/sbin/logrotate@root /usr/sbin/logrotate@' $BASE_DIR/crontab/kaltura_crontab
 configure_apache
-sed -i 's@^kaltura_activation_key\s*=\s*@kaltura_activation_key = YjI2MzgwMmUyMDA0ZTA1ODg1MWFjYWJiNDExMTEzNWV8MXxuZXZlcnww@' $BASE_DIR/app/configurations/local.ini
+set_serial
 configure_dwh
-echo "Creating test partner.."
-php $INSTALL_DIR/create_partner.php `echo "select admin_secret from partner where id=-2;" |mysql -u"$DB1_USER" -p"$DB1_PASS" -P$DB1_PORT $DB1_NAME |sed '1,1d'`
+for i in sphinx_watch.sh serviceBatchMgr.sh;do
+	chkconfig $i on
+done
+/etc/init.d/serviceBatchMgr.sh restart
+create_partner
+
