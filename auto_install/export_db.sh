7a9f6b31cbe451a3bcbee5db0453e7e13fff8d80
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-10-23 17:57:16 +0000

    qnd: export_db.sh - script to export and gzip each table under kaltura.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@89088 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ auto_install/export_db.sh
@@ -0,0 +1,40 @@
+#!/bin/bash - 
+#===============================================================================
+#          FILE: table_per_file.sh
+#         USAGE: ./table_per_file.sh 
+#   DESCRIPTION: 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jess Portnoy (), jess.portnoy@kaltura.com
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED: 08/10/12 18:07:22 IDT
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 <schema> <user> <passwd> <port> <output-path> [verbose]"
+    exit 1
+fi
+TABLE_SCHEMA=$1
+DBUSER=$2
+DBPASSWD=$3
+DBPORT=$4
+OUT="$5"
+TABLES=`mysql -u$DBUSER -p$DBPASSWD -P$DBPORT -B -N -e "select TABLE_NAME from information_schema.TABLES where TABLE_SCHEMA='$TABLE_SCHEMA'"`
+
+
+if [ ! -d $OUT ]; then
+	mkdir -p "$OUT"
+fi
+
+for TABLE in $TABLES; do
+	if [ -n "$6" ];then
+		echo -n "dumping $TABLE_SCHEMA.$TABLE..."
+	fi
+    	mysqldump -u$DBUSER -p$DBPASSWD -P$DBPORT $TABLE_SCHEMA $TABLE | gzip > $OUT/$TABLE_SCHEMA.$TABLE.sql.gz
+done
