53f4731628a0aed12ddce09b799afc6b2bb7ed77
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2012-11-11 18:10:11 +0000

    qnd: realigned menu items and numbers.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@90009 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- auto_install/main.sh
+++ auto_install/main.sh
@@ -5,11 +5,11 @@ probe_for_garbage
 cat << EOF 
 Welcome to Kaltura $KALT_VER setup tool
 Please select one of the following options:
-1. all in one install
-2. batch instance
-3. sphinx instance
-4. API machine
-5. export Kaltura's MySQL DBs
+1. batch instance
+2. sphinx instance
+3. API machine
+4. export Kaltura's MySQL DBs
+5. unistall
 6. configure Sphinx for this host
 7. configure DB for this host
 EOF
@@ -33,7 +33,7 @@ elif [ $CHOICE = 4 ];then
 	echo "About to export Kaltura's MySQL DB.."
 	export_mysql_kalt_db
 elif [ $CHOICE = 5 ];then
-	echo "About to export Kaltura's MySQL DB.."
+	echo "Uninstall"
 	`dirname $0`/cleanup.sh
 elif [ $CHOICE = 6 ];then
 	echo "About to configure Sphinx for this host.."
@@ -42,7 +42,7 @@ elif [ $CHOICE = 7 ];then
 	echo "About to configure MySQL DB for this host.."
 	set_mysqldb
 else
-	echo "Choose a value between 1-5"
+	echo "Choose a value between 1-7"
 	exit 1
 fi
 
