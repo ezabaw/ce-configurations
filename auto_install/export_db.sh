1ff04155f5d082916e32bb17a0724a908857b6e7
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-03 08:38:00 +0000

    qnd: get host in $@ - localhost is not really too useful now, is it?
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@92479 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- auto_install/export_db.sh
+++ auto_install/export_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash - 
 #===============================================================================
 #          FILE: ./export_db.sh
-#         USAGE: ./export_db.sh <schema> <user> <passwd> <port> <output/path> [verbose]
+#         USAGE: ./export_db.sh <schema> <user> <passwd> <port> <host> <output/path> [verbose]
 #   DESCRIPTION: 
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
@@ -16,16 +16,17 @@
 #set -o nounset                              # Treat unset variables as an error
 
 
-if [ $# -lt 5 ]; then
-    echo "Usage: $0 <schema> <user> <passwd> <port> <output/path> [verbose]"
+if [ $# -lt 6 ]; then
+    echo "Usage: $0 <schema> <user> <passwd> <port> <host> <output/path> [verbose]"
     exit 1
 fi
 TABLE_SCHEMA=$1
 DBUSER=$2
 DBPASSWD=$3
 DBPORT=$4
-OUT="$5"
-TABLES=`mysql -u$DBUSER -p$DBPASSWD -P$DBPORT -B -N -e "select TABLE_NAME from information_schema.TABLES where TABLE_SCHEMA='$TABLE_SCHEMA'"`
+DBHOST=$5
+OUT="$6"
+TABLES=`mysql -h$DBHOST -u$DBUSER -p$DBPASSWD -P$DBPORT -B -N -e "select TABLE_NAME from information_schema.TABLES where TABLE_SCHEMA='$TABLE_SCHEMA'"`
 
 
 if [ ! -d $OUT ]; then
@@ -33,8 +34,8 @@ if [ ! -d $OUT ]; then
 fi
 
 for TABLE in $TABLES; do
-	if [ -n "$6" ];then
+	if [ -n "$7" ];then
 		echo -n "dumping $TABLE_SCHEMA.$TABLE... to $OUT"
 	fi
-    	mysqldump -u$DBUSER -p$DBPASSWD -P$DBPORT --routines --single-transaction $TABLE_SCHEMA $TABLE | gzip > $OUT/$TABLE_SCHEMA.$TABLE.sql.gz
+    	mysqldump -h$DBHOST -u$DBUSER -p$DBPASSWD -P$DBPORT --routines --single-transaction $TABLE_SCHEMA $TABLE | gzip > $OUT/$TABLE_SCHEMA.$TABLE.sql.gz
 done
