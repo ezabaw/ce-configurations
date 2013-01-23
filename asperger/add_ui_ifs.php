d971718959818263ccc4fc7c36a8b4ba85802e7f
jessp <jessp@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-01-23 13:58:29 +0000

    qnd: BE support for UI IFs insertion.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@93256 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ asperger/add_ui_ifs.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (empty($_POST['admin_console_url']) && empty($_POST['kmc_url'])&& empty($_POST['kms_url'])){
+    die('At least one URL is mandatory.');
+}
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'conn.inc');
+if (!isset($_SESSION['asper_session']) || !$_SESSION['asper_session']){
+    require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'validate_session.inc');
+}
+
+$env=$_POST['env'];
+$admin_console_url=$_POST['admin_console_url'];
+$admin_console_user=SQLite3::escapeString($_POST['admin_console_user']);
+$admin_console_passwd=SQLite3::escapeString($_POST['admin_console_passwd']);
+$kmc_url=SQLite3::escapeString($_POST['kmc_url']);
+$kmc_user=SQLite3::escapeString($_POST['kmc_user']);
+$kmc_passwd=SQLite3::escapeString($_POST['kmc_passwd']);
+$kms_admin_url=SQLite3::escapeString($_POST['kms_admin_url']);
+$kms_admin_user=SQLite3::escapeString($_POST['kms_admin_user']);
+$kms_admin_passwd=SQLite3::escapeString($_POST['kms_admin_passwd']);
+$notes=SQLite3::escapeString($_POST['notes']);
+$customer_id=SQLite3::escapeString($_POST['customer_id']);
+$db=new SQLite3($dbfile) or die("Unable to connect to database $dbfile");
+$query="insert into ui values(NULL,$customer_id,'$admin_console_url','$admin_console_user','$admin_console_passwd','$kmc_url','$kmc_user','$kmc_passwd','$kms_admin_url','$kms_admin_user','$kms_admin_passwd','$notes','$env')";
+error_log($query."\n",3,'/tmp/que');
+$db->exec($query);
+if ($db->lastErrorCode()){
+    $msg=json_encode('ERROR: #' . $db->lastErrorCode() . ' '.$db->lastErrorMsg().' :(');
+}else{
+    $msg="Record added successfully to ui.";
+}
+$db->close();
+echo $msg;
+?>
