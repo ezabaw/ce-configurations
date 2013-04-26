4b53f1b967b013a87799b4f33bcef09e367b524f
christopherd <christopherd@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-04-26 18:41:14 +0000

    qnd: updated script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/trunk/on-premise/all@97324 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/kaltura_info.sh
+++ scripts/kaltura_info.sh
@@ -7,9 +7,8 @@
 # general cleanup
 # convert to printf so I look cool in the bash scene
 usage () {
-	echo -e "Usage: $0 -s <kaltura_directory> -o <report_output_directory>
-			Optional: -c  performs md5 checksum as part of the report
-			-m <email> sends out the report to the supplied email address\n"
+	echo -e "Usage: $0 -b <kaltura_directory>"
+		
 }
 
 while :
@@ -19,21 +18,9 @@ do
             usage
 			exit 0
 			;;
-		-s | --basedir)
-			$base_dir=$2
+		-b | --basedir)
+			base_dir=$2
 			shift 2
-			;;
-		-o | --reportdir)
-			$report_dir=$2/kalturareport
-			shift 2
-			;;
-		-m | --mail)
-			$report_email=$2
-			shift 2
-			;;
-		-c	| --checksum)
-			$checksum=1
-			shift
 			;;
         *)  
             break
@@ -41,63 +28,25 @@ do
     esac
 done
 
-# required paramters
-if [ -z $report_dir ];then
-	echo "Missing output directory"
-	usage
-	exit 1
-fi
+
 if [ -z $base_dir ];then
-	echo "Missing base directory"
-	usage
-	exit 1
+	echo "Base directory not specificed assuming /opt/kaltura"
+	base_dir="/opt/kaltura"
 fi
 
-# Utility functions
+# Utility functions, useful to extract parameters from the kaltura variables
 pextract () {
 	returnval=$(echo $@  | awk 'BEGIN { FS="=|=>" } { print $2 }' | tr -d ' |" | ,')
 }
 
-
-
-# Determine Kaltura version
+# Determine Kaltura version, this needs to be changed to something better
 if [ -e $base_dir/app/configurations/version.ini ];then
         version=6
+        echo "Kaltura Falcon(2) Detected"
 else
         version=5
+        echo "Kaltura Eagle Detected"
 fi
-
-# Gather configuration files
-mkdir -p $report_dir/config
-mkdir -p $report_dir/etc/sysconfig/network-scripts
-if [ $version -eq 6 ];then
-	cp -a $base_dir/app/configurations/* $report_dir/config
-elif [ $version -eq 5 ]; then
-	mkdir -p $report_dir/config/api_v3
-	mkdir -p $report_dir/config/batch
-	mkdir -p $report_dir/config/alpha
-	mkdir -p $report_dir/config/admin_console
-	cp -a $base_dir/app/api_v3/config/* $report_dir/config/api_v3/
-	cp -a $base_dir/app/batch/*.ini $report_dir/config/batch/ 
-	cp -a $base_dir/app/alpha/config/* $report_dir/config/alpha/
-	cp -a $base_dir/app/admin_console/configs/* $report_dir/config/admin_console
-else
-	echo "Version unsupported"	
-	exit 1
-fi
-ps aux >> $report_dir/process_report
-netstat -anp >> $report_dir/network_report
-
-# Copy configuration files to the report directory
-cp -a /etc/hosts $report_dir/etc/hosts
-cp -a /etc/my.cnf $report_dir/etc/my.cnf
-cp -a /etc/php.ini $report_dir/etc/php.ini
-cp -aL /etc/php.d $report_dir/etc/
-mkdir -p $report_dir/httpd/
-cp -aL /etc/httpd/conf.d $report_dir/httpd/conf.d
-cp -aL /etc/httpd/conf $report_dir/httpd/conf
-cp -aL /etc/sysconfig/network $report_dir/etc/sysconfig/
-cp -aL /etc/sysconfig/network-scripts $report_dir/etc/sysconfig/
 
 # System information
 mysql_version=$(mysql --version | awk '{ print $5 }' |cut -f 1 -d ',')
@@ -109,38 +58,42 @@ total_memorykb=$(grep 'MemTotal' /proc/meminfo | awk '/^MemTotal/{print $2}')
 total_memory=$(expr $total_memorykb / 1024)
 free_space=$(df -P | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }')
 free_inodes=$(df -iP | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }')
+ips=$(ifconfig | grep -oP 'inet addr:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+# php timezone
 pextract $(grep -i '^date.timezone' /etc/php.ini)
 php_timezone=$returnval
 
 # Kaltura information
 if [ $version -eq 6 ];then
-    pextract $(grep -m 1 'settings.serviceUrl' $report_dir/config/admin.ini)
+    pextract $(grep -m 1 'settings.serviceUrl' $base_dir/app/configurations/admin.ini)
     serviceUrl=$returnval
-    pextract $(grep -m 1 'id' $report_dir/config/batch.ini)
+    pextract $(grep -m 1 'id' $base_dir/app/configurations/admin.ini)
 	batchID=$returnval
-	pextract $(grep -m 1 'datasources.propel.connection.user' $report_dir/config/db.ini)
+	pextract $(grep -m 1 'datasources.propel.connection.user' $base_dir/app/configurations/db.ini)
 	dbuser=$returnval
-	pextract $(grep -m 1 'datasources.propel.connection.password' $report_dir/config/db.ini)
+	pextract $(grep -m 1 'datasources.propel.connection.password' $base_dir/app/configurations/db.ini)
 	dbpass=$returnval
-	pextract $(grep -m 1 'datasources.propel.connection.hostspec' $report_dir/config/db.ini)
+	pextract $(grep -m 1 'datasources.propel.connection.hostspec' $base_dir/app/configurations/db.ini)
 	dbhost=$returnval
-	sphinx_host=$(grep 'datasources.sphinx.connection.dsn' $report_dir/config/db.ini |cut -f 3 -d'='|cut -f 1 -d';')
+	sphinx_host=$(grep 'datasources.sphinx.connection.dsn' $base_dir/app/configurations/db.ini |cut -f 3 -d'='|cut -f 1 -d';')
+	# Kaltura timezone
 	pextract $(grep -i '^date_default_timezone' $base_dir/app/configurations/local.ini)
 	kaltura_timezone=$returnval
+	# Kettle timezone
 	pextract $(grep '^DataTimeZone' $base_dir/dwh/.kettle/kettle.properties)
 	kettle_timezone=$returnval
 elif [ $version -eq 5 ]; then
-	pextract $(grep -m 1 'setting' $report_dir/config/admin_console/application.ini)
+	pextract $(grep -m 1 'setting' $base_dir/app/config/admin_console/application.ini)
 	serviceUrl=$returnval
-	pextract $(grep -m 1 'id' $report_dir/config/batch/batch_config.ini)
+	pextract $(grep -m 1 'id' $base_dir/app/batch/batch_config.ini)
 	batchID=$returnval
-	dbuser=$(grep -m 1 "'user'"  $report_dir/config/alpha/kConfLocal.php \
+	dbuser=$(grep -m 1 "'user'"  $base_dir/app/api_v3/config/alpha/kConfLocal.php \
 	| awk '{print $3}' | cut -f 2 -d"'")
-	dbpass=$(grep -m 1 "'password'" $report_dir/config/alpha/kConfLocal.php \
+	dbpass=$(grep -m 1 "'password'" /app/api_v3/app/alpha/config/kConfLocal.php \
        	| awk '{print $3}' | cut -f 2 -d"'")
-	dbhost=$(grep -m 1 "'hostspec'" $report_dir/config/alpha/kConfLocal.php \
+	dbhost=$(grep -m 1 "'hostspec'" $base_dir/app/alpha/config/kConfLocal.php \
        	| awk '{print $3}' | cut -f 2 -d"'")
-	sphinx_host=$(grep  -o -m 1 -A 5 "mysql:host=.*\;port=" $report_dir/config/alpha/kConfLocal.php  | awk -F '(mysql:host=|;port=)' '{print $2}')
+	sphinx_host=$(grep  -o -m 1 -A 5 "mysql:host=.*\;port=" $base_dir/app/alpha/config/kConfLocal.php  | awk -F '(mysql:host=|;port=)' '{print $2}')
 	pextract $(grep -i 'date_default_timezone' $base_dir/app/alpha/config/kConfLocal.php)
 	kaltura_timezone=$returnval
 	pextract $(grep '^DataTimeZone' $base_dir/dwh/.kettle/kettle.properties)
@@ -151,16 +104,17 @@ else
 fi
 
 # Report generation
-cat > $report_dir/system_report <<EOL
+cat > /tmp/ksystem_report <<EOL
 ----------------------------------------------
 Kaltura Installation Report Tool
 $(date)
-----------------------------------------------
+
 Processor(s): ${cpu_speed}Mhz x $cpu_cores cores  Memory: ${total_memory}MB
 Status: $(uptime)
 Hostname: $(hostname)
 Disk Usage:
 $free_space
+
 Inode Usage:
 $free_inodes
 
@@ -169,98 +123,85 @@ Kaltura Base Directory: $base_dir
 Kaltura Log Directory Size: $(du -sh $base_dir/log | awk {'print $1'})
 Kaltura Web Log Directory Size: $(du -sh $base_dir/web/logs 2> /dev/null| awk '{print $1}')
 Service URL: $serviceUrl
-Batch ID: $batchID
 
-MySQL User: $dbuser
-MySQL Password: $dbpass
+$ips
+MySQL User: $dbuser Password: $dbpass
 MySQL Host: $dbhost
 Sphinx Host: $sphinx_host
 
 Time zones:
 PHP: $php_timezone
-Kaltura: $kaltura_timezone
-Kettle: $kettle_timezone
-
+ Kaltura: $kaltura_timezone 
+ Kettle: $kettle_timezone
 EOL
 
+# Time check
+if ntpstat &> /dev/null ;then
+	echo "The NTP server is functioning properly, clock syncronized" >> /tmp/ksystem_report
+else
+	echo "There is a problem with the NTP server" >> /tmp/ksystem_report
+fi
 # MySQL connection check
-echo -n "MySQL Connection: " >> $report_dir/system_report
+echo -en "\nMySQL Connection: " >> /tmp/ksystem_report
 
 if mysql -u $dbuser -p$dbpass -h $dbhost <<< "quit" &> /dev/null ;then
-	echo -e "\033[32mSuccessful \033[0m" >> $report_dir/system_report
+	echo -e "\033[32mSuccessful \033[0m" >> /tmp/ksystem_report
 else
-	echo -e "\033[31mFailed \033[0m" >> $report_dir/system_report
+	echo -e "\033[31mFailed \033[0m" >> /tmp/ksystem_report
 fi
 
 # Sphinx connection check
-echo -n "Sphinx Connection: " >> $report_dir/system_report
+echo -n "Sphinx Connection: " >> /tmp/ksystem_report
 if nc -z -w 2 $sphinx_host 9312 &> /dev/null ;then
-	echo -e "\033[32mSuccessful \033[0m" >> $report_dir/system_report
+	echo -e "\033[32mSuccessful \033[0m" >> /tmp/ksystem_report
 else
-	echo -e "\033[31mFailed \033[0m" >> $report_dir/system_report
+	echo -e "\033[31mFailed \033[0m" >> /tmp/ksystem_report
 fi
 
 # Basic API check, the local connection is made to the current machine, whereas the remote
-# connection is the configuraitons service url
-echo -n "Local API Connection: " >> $report_dir/system_report
+# connection is the configuraitons service url, the local machine may not be acting as an API
+# server
+echo -n "Local API Connection: " >> /tmp/ksystem_report
 api_test=$(wget -qO- --tries=1 --timeout=10 'http://localhost/api_v3/?service=system&action=ping' | awk  -F '(<result>|</result>)' '{print $2}')
 if [ -z "$api_test" ] || [ "$api_test" != "1" ];then
-	echo -e "\033[31mFailed \033[0m" >> $report_dir/system_report
+	echo -e "\033[31mFailed \033[0m" >> /tmp/ksystem_report
 else
-	echo -e "\033[32mSuccessful\033[0m" >> $report_dir/system_report
+	echo -e "\033[32mSuccessful\033[0m" >> /tmp/ksystem_report
 fi
-echo -n "Remote API Connection: " >> $report_dir/system_report
+echo -n "Remote API Connection: " >> /tmp/ksystem_report
 remote_api_test=$(wget -qO- --tries=1 --timeout=10 "${serviceUrl}/api_v3/?service=system&action=ping" | awk  -F '(<result>|</result>)' '{print $2}')
 if [ -z "$api_test" ] || [ "$api_test" != "1" ];then
-	echo -e "\033[31mFailed \033[0m" >> $report_dir/system_report
+	echo -e "\033[31mFailed \033[0m" >> /tmp/ksystem_report
 else
-	echo -e "\033[32mSuccessful\033[0m" >> $report_dir/system_report
+	echo -e "\033[32mSuccessful\033[0m" >> /tmp/ksystem_report
 fi
 
 # Check if services are running
-echo -n "Services: " >> $report_dir/system_report
+echo -n "Services: " >> /tmp/ksystem_report
 for x in searchd httpd mysqld memcached KGenericBatchMgr ntp;do
         if pgrep -of $x >> /dev/null;then
-                echo -ne "\033[32m${x} \033[0m" >> $report_dir/system_report
+                echo -ne "\033[32m${x} \033[0m" >> /tmp/ksystem_report
         else
-                echo -ne "\033[31m${x} \033[0m" >> $report_dir/system_report
+                echo -ne "\033[31m${x} \033[0m" >> /tmp/ksystem_report
         fi
 done
 
+# Test NTP
+
+
+
 # Software versions
-echo -e "\nPHP $php_version Apache $apache_version MySQL $mysql_version\n" >> $report_dir/system_report
+echo -e "\n\nPHP $php_version Apache $apache_version MySQL $mysql_version" >> /tmp/ksystem_report
 
 # Log status
-echo -e "\n Log status" >> $reportdir/system_report
+echo -e "\n Log status" >> /tmp/ksystem_report
 if [ $version -eq 6 ];then
-	grep '^[^;]*priority \?= \?[0-9]' /opt/kaltura/app/configurations/logger.ini >> $report_dir/system_report
+	grep -H '^[^;]*priority \?= \?[0-9]' /opt/kaltura/app/configurations/logger.ini >> /tmp/ksystem_report
 else 
-	grep '^[^;]*priority \?= \?[0-9]' /opt/kaltura/app/batch/logger.ini /opt/kaltura/app/api_v3/config/logger.ini >> $report_dir/system_report
+	grep -H '^[^;]*priority \?= \?[0-9]' /opt/kaltura/app/batch/logger.ini /opt/kaltura/app/api_v3/config/logger.ini >> /tmp/ksystem_report
 fi
 # Output report
-cat $report_dir/system_report
+cat /tmp/ksystem_report
 
-# Mail the report to the report user
-if [ ! -z $report_email ];then
-	archive_file="/tmp/kaltura_report_$(date +%Y-%m-%d-%H-%M).tar.gz"
-	tar czf "$archive_file" "$report_dir" &> /dev/null
-	gpg --batch --passphrase kimberbenton -c "$archive_file" &> /dev/null
-	mailx -s "Kaltura report from $(hostname)" -a "$archive_file" "$report_email" < $report_dir/system_report
-fi
-# Perform checksum
-if [ ! -z $checksum ];then
-	echo -e "Performing checksum of the kaltura directory" | tee -a $report_dir/md5sum
-	./md5compare.sh $base_dir /tmp/md5report
-	# Run comparison check
-	if [ $version -eq 6 ];then
-		php md5compare.php falcon.md5 /tmp/md5report
-	elif [ $version -eq 5 ];then
-		php md5compare.php eagle.md5 /tmp/md5report
-	fi
-	echo "Missing count $(wc -l /tmp/md5missing)"
-	echo "Additional count $(wc -l /tmp/md5added)"
-	echo "Modified $(wc -l /tmp/md5changed)"
-	echo "Checksum complete" | tee -a $report_dir/md5sum
-fi
 
 exit 0
